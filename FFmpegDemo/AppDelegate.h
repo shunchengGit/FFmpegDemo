c05f14790c8006b0c614f5c676ec2a55cdf255d7
chengshun <chengshun@meituan.com>
2018-06-14 16:06:32 +0800

    Create Project

---
+++ FFmpegDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FFmpegDemo
+//
+//  Created by chengshun on 2018/6/14.
+//  Copyright © 2018年 chengshun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FFmpegDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FFmpegDemo
+//
+//  Created by chengshun on 2018/6/14.
+//  Copyright © 2018年 chengshun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
